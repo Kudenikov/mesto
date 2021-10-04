3a389d3bcd688bb2ee0f2492c51d777051b93617
Artem Kudenikov <a.kudenikov@yandex.ru>
2021-10-04 23:39:30 +0300

    Исправлены ошибки после 1 итерации

--- script/index.js
+++ script/index.js
@@ -3,11 +3,10 @@ const popupCloseButton = document.querySelector('.popup__close');
 const editButton = document.querySelector('.profile__edit-button');
 const form = document.querySelector('.popup__form');
 
-const nameField = document.querySelector('.popup__input_name');
-const professionField = document.querySelector('.popup__input_profession');
+const nameField = document.querySelector('input[name="profile-name"]');
+const professionField = document.querySelector('input[name="profile-profession"]');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession')
-const likeButton = document.querySelectorAll('.cards__heart-pic');
 
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -26,15 +25,6 @@ function submitForm(event) {
     closePopup();
 }
 
-for (let i=0; i<likeButton.length; i++) {
-    likeButton[i].addEventListener('click', function() {
-        if (likeButton[i].getAttribute('src') === './images/heart.svg') {
-        likeButton[i].setAttribute('src', './images/heart-active.svg')}
-        else {
-            likeButton[i].setAttribute('src', './images/heart.svg')
-        }
-    })
-}
 editButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 form.addEventListener('submit', submitForm);
