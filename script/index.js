fe2369ead7d1ea89a99784ba364db903c62d6679
Artem Kudenikov <a.kudenikov@yandex.ru>
2021-10-16 19:48:33 +0300

    feature #1 cards created by js

--- script/index.js
+++ script/index.js
@@ -8,6 +8,50 @@ const professionField = document.querySelector('input[name="profile-profession"]
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession')
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const cards = document.querySelector('.cards');
+const templateItem = document.querySelector('.template').content;
+
+initialCards.forEach(appendCard);
+
+function appendCard(item) {
+    const card = createCard(item);
+    cards.append(card);
+}
+
+function createCard(item) {
+    const card = templateItem.querySelector('.cards__item').cloneNode(true);
+    card.querySelector('.cards__place').innerText = item.name;
+    card.querySelector('.cards__image').src = item.link;
+    return card;
+}
+
 function openPopup() {
     popup.classList.add('popup_opened');
     nameField.value = profileName.textContent;
